9696acc6f50319898fb675978425b8cc2c81224f
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-26 14:23:02 -0400

    Add database schema and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+greetingOne = Greeting.create(salutation: "Hello")
+greetingtwo = Greeting.create(salutation: "Hi")
+greetingThree = Greeting.create(salutation: "Good morning")
+greetingFour = Greeting.create(salutation: "Good evening")
+greetingFive = Greeting.create(salutation: "Good afternoon")
