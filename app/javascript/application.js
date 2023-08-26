90d492b5d4d82858c0c39ecd187a90e75f5daae1
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-26 12:10:41 -0400

    Update application. js Add root to render greetings

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,25 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+import "@hotwired/turbo-rails";
+import "./controllers";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from "./redux/store/store";
+import App from "./components/App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </React.StrictMode>,
+);
