86b62808649c9f7181a6d0f7295c895e75a35375
Fredo St Fleur <fredostfleur23@gmail.com>
2023-08-26 11:40:01 -0400

    - Add store -Add greetingSlice for fetch Api

---
+++ app/javascript/redux/store/greetingSlice.js
@@ -0,0 +1,38 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const initialState = {
+    greetings: [],
+    loading: false,
+    error: '',
+};
+
+const url = 'http://localhost:3000/api/v1/greetings';
+
+
+export const getgreetings = createAsyncThunk('greetings/fetchgreetings', async () => {
+    const response = await fetch(`${url}`);
+    const data = await response.json();
+    return data;
+});
+
+const greetingsSlice = createSlice({
+    name: 'greetings',
+    initialState,
+    extraReducers: (builder) => {
+        builder.addCase(getgreetings.pending, (state) => {
+            state.loading = true;
+        });
+        builder.addCase(getgreetings.fulfilled, (state, action) => {
+            state.loading = false;
+            state.greetings = action.payload;
+            state.error = '';
+        });
+        builder.addCase(getgreetings.rejected, (state, action) => {
+            state.loading = false;
+            state.greetings = [];
+            state.error = action.error.message;
+        });
+    },
+});
+
+export default greetingsSlice.reducer;
